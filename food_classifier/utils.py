730682cb52f4741a724bdb73f523818ea48d7430
teudjio <teudjiombativou@gmail.com>
2018-01-20 15:17:38 +0100

    add utils.py module

---
+++ food_classifier/utils.py
@@ -0,0 +1,15 @@
+import errno
+import os
+
+__author__ = 'Junior Teudjio'
+__all__ = ['mkdir_p']
+
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
