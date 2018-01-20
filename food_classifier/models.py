3e520ef6e35297d5423f33969ef3f00b7db67569
teudjio <teudjiombativou@gmail.com>
2018-01-20 19:18:23 +0100

    add regularization to weights

--- food_classifier/models.py
+++ food_classifier/models.py
@@ -1,4 +1,5 @@
 import logging
+import functools
 
 from keras.models import Sequential
 from keras import layers
@@ -16,7 +17,7 @@ __author__ = 'Junior Teudjio'
 __all__ = ['base_model', 'vgg16_model', 'mobilenet_model']
 
 
-def base_model(init='glorot_uniform', activation='relu', batch_norm=True, dropout=0.5):
+def base_model(init='glorot_uniform', activation='relu', batch_norm=True, dropout=0.5, regularizer='l2-0.01'):
     '''
     Create an instance of the baseline model.
 
@@ -30,31 +31,40 @@ def base_model(init='glorot_uniform', activation='relu', batch_norm=True, dropou
         Whether to use batch normalization or not.
     dropout : float
         Ratio of weights to turn off before the final activation function
+    regularizer: str : reg_type-reg_value
+        Type and value of regularization to use, reg_type = l2 or l1
 
     Returns:
         model: Sequential()
             The baseline model object
     '''
+    reg_type, reg_value = regularizer.split('-')
+    if reg_type == 'l2':
+        regularizer = layers.regularizers.l2(float(reg_value))
+    else:
+        regularizer = layers.regularizers.l1(float(reg_value))
+    myConv2D = functools.partial(layers.Conv2D, kernel_initializer=init, kernel_regularizer=regularizer)
+    
     model = Sequential()
-    model.add(layers.Conv2D(32, (3, 3), input_shape=(150, 150, 3), kernel_initializer=init))
+    model.add(myConv2D(32, (3, 3), input_shape=(150, 150, 3)))
     if batch_norm:
         model.add(BatchNormalization())
     model.add(layers.Activation(activation))
     model.add(layers.MaxPooling2D((2, 2)))
 
-    model.add(layers.Conv2D(64, (3, 3), kernel_initializer=init))
+    model.add(myConv2D(64, (3, 3)))
     if batch_norm:
         model.add(BatchNormalization())
     model.add(layers.Activation(activation))
     model.add(layers.MaxPooling2D((2, 2)))
 
-    model.add(layers.Conv2D(128, (3, 3), kernel_initializer=init))
+    model.add(myConv2D(128, (3, 3)))
     if batch_norm:
         model.add(BatchNormalization())
     model.add(layers.Activation(activation))
     model.add(layers.MaxPooling2D((2, 2)))
 
-    model.add(layers.Conv2D(128, (3, 3), kernel_initializer=init))
+    model.add(myConv2D(128, (3, 3)))
     if batch_norm:
         model.add(BatchNormalization())
     model.add(layers.Activation(activation))
@@ -63,7 +73,7 @@ def base_model(init='glorot_uniform', activation='relu', batch_norm=True, dropou
 
     model.add(layers.Flatten())
     model.add(layers.Dropout(dropout))
-    model.add(layers.Dense(512, activation=activation))
+    model.add(layers.Dense(512, activation=activation, kernel_initializer=init, kernel_regularizer=regularizer))
     model.add(layers.Dense(1, activation='sigmoid'))
 
     logger.info('''
@@ -89,3 +99,6 @@ def vgg16_model():
 
 def mobilenet_model():
     pass
+
+if __name__ == '__main__':
+    base_model().summary()
