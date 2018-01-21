2a689bf86ef2735903ae04c654e876f5a0906c29
teudjio <teudjiombativou@gmail.com>
2018-01-21 03:30:48 +0100

    add entry points scripts

--- food_classifier/run__food_classifier_service.sh
+++ food_classifier/run__food_classifier_service.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+CODE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+export PYTHONPATH=$PYTHONPATH:$CODE_DIR
+export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
+
+# script to serve the model as a flask rest api
+python2 $CODE_DIR/utils/api.py
--- food_classifier/run__preprocess_and_train.sh
+++ food_classifier/run__preprocess_and_train.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+CODE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+export PYTHONPATH=$PYTHONPATH:$CODE_DIR
+export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
+
+pip install -r $CODE_DIR/requirements.txt
+
+# script to download and preprocess the dataset (mainly splitting into train, validation and test sets)
+python2 $CODE_DIR/dataset_preprocess.py
+# script to train the model
+python2 $CODE_DIR/utils/train.py
+
