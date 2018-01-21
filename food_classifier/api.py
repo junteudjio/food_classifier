0157c022de58515329b7ab632d634c38bc1ac1ac
teudjio <teudjiombativou@gmail.com>
2018-01-21 07:31:31 +0100

    add inference model; add flask api for inference model

--- food_classifier/api.py
+++ food_classifier/api.py
@@ -1,1 +1,30 @@
+import argparse
+
+from flask import Flask, jsonify, request
+
+import inference
+
+
 __author__ = 'Junior Teudjio'
+
+
+app = Flask(__name__)
+inference_model = inference.Model()
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+     request_args = request.args
+     prediction = inference_model.predict(image_path=request_args['image_path'],
+                                          model_type=request_args['model_type'])
+     return jsonify(prediction)
+
+def _setup_args():
+     parser = argparse.ArgumentParser()
+     parser.add_argument('--port', type=int, default=8086)
+     parser.add_argument('--host', type=str, default='0.0.0.0')
+     return parser.parse_args()
+
+if __name__ == '__main__':
+     args = _setup_args()
+     app.run(port=args.port,host=args.host)
--- food_classifier/inference.py
+++ food_classifier/inference.py
@@ -1,1 +1,72 @@
+from __future__ import division
+import os
+import glob
+import logging
+
+from PIL import Image
+import numpy as np
+import keras
+from keras.preprocessing.image import img_to_array, load_img
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+file_handler = logging.FileHandler('./logs/inference.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 __author__ = 'Junior Teudjio'
+
+ckpts_path = 'model_ckpts/_backups'
+
+def _get_best_model_path(model_type):
+    model_type_template = '{}.ckpt-*'.format(model_type)
+    all_models_paths = glob.glob(os.path.join(ckpts_path, model_type_template))
+    all_models_paths.sort(key=lambda path: path.split('-')[-1]) #sort by val_score
+
+    best_model_path = all_models_paths[-1]
+    best_model = keras.models.load_model(best_model_path)
+
+    return best_model
+
+class Model(object):
+    def __init__(self):
+        self.models = dict()
+        try:
+            self.models['base_model'] = _get_best_model_path('base_model')
+        except Exception as e:
+            logger.error('Unable to load model, error_message = {}'.format(repr(e)))
+            raise e
+        #TODO: uncomment when model is implemented and trained
+        #self.models['mobilenet_model'] = _get_best_model_path('mobilenet_model')
+        self.labels = {1:'sandwich', 0:'sushi'}
+
+    def predict(self, image_path, model_type='base_model'):
+        logger.info('''
+        Getting prediction for
+        image: {image_path}
+        model_type: {model_type}'''.format(image_path=image_path, model_type=model_type))
+        img = load_img(image_path)  # this is a PIL image
+        if model_type == 'base_model':
+            resize_shapes = (150, 150)
+        else:
+            resize_shapes = (160, 160) # excepted size for mobilenet_model
+        img = img.resize(resize_shapes,  Image.ANTIALIAS)
+        array = img_to_array(img) * 1./255 # this is a Numpy array with shape (3, 150, 150)
+        array = array[np.newaxis, ...] # add another dimension for batch
+
+        proba = self.models[model_type].predict_proba(array)[0][0]
+        label_idx = 0 if proba >= 0.5 else 1
+        confidence = proba if label_idx == 0 else 1. - proba
+
+        prediction = {
+            'label_idx': str(label_idx),
+            'label':self.labels[label_idx],
+            'confidence': str(confidence)
+        }
+
+        logger.info('''
+        Prediction: {}'''.format(str(prediction)))
+
+        return prediction
