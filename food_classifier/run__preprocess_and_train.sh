ab6a26c1bc8a40ce6e8af0095ff2385153017008
teudjio <teudjiombativou@gmail.com>
2018-01-22 00:31:38 +0100

    +x to .sh files; update flask version

--- food_classifier/run__preprocess_and_train.sh
+++ food_classifier/run__preprocess_and_train.sh
@@ -8,6 +8,21 @@ export PYTHONPATH=$PYTHONPATH:$ROOT_DIR
 
 pip install -r $CODE_DIR/requirements.txt
 
+# create all the necessary directories
+DATA_DIR=data
+DOWNLOAD_DIR=downloads
+PLOTS_DIR=history_plots
+LOGS_DIR=logs
+MODEL_CKPTS_DIR=model_ckpts
+TEST_LOGS_DIR=../tests/logs
+
+mkdir -p $DATA_DIR
+mkdir -p $DOWNLOAD_DIR
+mkdir -p $PLOTS_DIR
+mkdir -p $LOGS_DIR
+mkdir -p $MODEL_CKPTS_DIR
+mkdir -p $TEST_LOGS_DIR
+
 # script to download and preprocess the dataset (mainly splitting into train, validation and test sets)
 python2 $CODE_DIR/data_preprocess.py
 # script to train the model
